8fb682b285c720a915a1ec29f9acd1f389788d1b
divyabhosale <d_bhosal@encs.concordia.ca>
2020-12-05 14:52:40 -0500

    Update settings.py

--- AuctionSystem/settings.py
+++ AuctionSystem/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n*0_d+53%iaybpk-j1e@a3)%1112#gm7af*jxe=t7u8*&ns55i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
